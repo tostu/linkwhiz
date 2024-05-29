84e477ceb5150464454319f1f186662e956c526b
tostu <torge.stubbe@googlemail.com>
2024-05-30 00:04:02 +0200

    custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,36 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        linkWhiz: {
+          "primary": "#2563eb",
+          "primary-content": "#f3f4f6",
+          "secondary": "#4791FF",
+          "secondary-content": "#f3f4f6",
+          "accent": "#FFD700",
+          "accent-content": "#1f2937",
+          "neutral": "#F9F9FB",
+          "neutral-content": "#151515",
+          "base-100": "#F4F4F6",
+          "base-200": "#d4d4d6",
+          "base-300": "#b5b5b7",
+          "base-content": "#141415",
+          "info": "#17A2B8",
+          "info-content": "#f3f4f6",
+          "success": "#28A745",
+          "success-content": "#f3f4f6",
+          "warning": "#FFC107",
+          "warning-content": "#140c00",
+          "error": "#DC3545",
+          "error-content": "#f3f4f6",
+        },
+      },
+    ],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
 }
 
