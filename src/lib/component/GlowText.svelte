1125a75bd33a179a86e7ad891246ff37c611b149
tostu <torge.stubbe@googlemail.com>
2024-06-01 22:49:59 +0200

    improve page + glowing hero text

---
+++ src/lib/component/GlowText.svelte
@@ -0,0 +1,18 @@
+<div class="inline-flex items-center justify-center">
+	<span
+		class="text-gradient absolute mx-auto box-content flex w-fit select-none border py-4 text-center font-extrabold blur-xl"
+	>
+		<slot></slot>
+	</span>
+	<span
+		class="text-gradient relative top-0 flex h-auto w-fit select-auto items-center justify-center py-4 text-center font-extrabold text-transparent"
+	>
+		<slot></slot>
+	</span>
+</div>
+
+<style lang="postcss">
+	.text-gradient {
+		@apply bg-gradient-to-r from-[#0087d9] to-[#6fbbfd] bg-clip-text text-transparent;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,26 @@
 	import type { PageData, ActionData } from './$types';
 
 	import Magic from 'virtual:icons/jam/magic';
+	import GlowText from '$lib/component/GlowText.svelte';
 	export let form: ActionData;
+
+	let copied = false;
+
+	async function copyLink() {
+		try {
+			await navigator.clipboard.writeText(form?.destination);
+			copied = true;
+			hideTimer();
+		} catch (err) {
+			console.error('Failed to copy text: ', err);
+		}
+	}
+
+	function hideTimer() {
+		setTimeout(function () {
+			copied = false;
+		}, 2000);
+	}
 </script>
 
 <svelte:head>
@@ -10,31 +29,57 @@
 	<meta name="description" content="WebSqueeze link shortener" />
 </svelte:head>
 
-<div class="flex min-h-full flex-grow flex-col items-center justify-center">
-	<div class="relative w-full">
-		<form action="POST" class="flex gap-3">
-			<input name="link" class="input input-primary w-full" placeholder="Shorten..." />
-			<button type="submit" class="btn btn-primary">
+<div class="flex min-h-full flex-grow flex-col items-center justify-center pb-16">
+	<div class="relative flex w-full flex-col gap-y-14">
+		<div>
+			<h1 class="text-center text-4xl font-extrabold">
+				<GlowText>Transform</GlowText>
+				Your Links with a Touch of
+				<GlowText>Magic</GlowText>
+			</h1>
+		</div>
+		<form method="POST" action="/" class="flex gap-3">
+			<input
+				name="link"
+				title="valid url"
+				pattern="\b(?:https?://)?(?:www\.)?\S+\.\S+\b"
+				class="input input-secondary w-full"
+				placeholder="Shorten..."
+			/>
+			<button type="submit" class="btn btn-primary btn-secondary">
 				<Magic style="font-size: 1.5em; color: white"></Magic>
 			</button>
 		</form>
 
-		{#if true}
-			<div role="alert" class="alert alert-info absolute top-16 shadow-lg">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					class="h-6 w-6 shrink-0 stroke-current"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-					></path></svg
+		{#if form?.success}
+			<div role="alert" class="alert bg-base-300 py-6 shadow-lg">
+				<div class="hidden sm:block">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						class="h-6 w-6 shrink-0 stroke-current"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+						></path></svg
+					>
+				</div>
+
+				<a class="link-accent" href={form?.destination}>{form?.destination}</a>
+				<button
+					class="btn btn-neutral btn-sm w-full sm:w-14"
+					class:btn-success={copied}
+					on:click={() => copyLink()}
 				>
-				<span>New software update available.</span>
-				<button class="btn btn-sm">Copy</button>
+					{#if copied}
+						OK
+					{:else}
+						Copy
+					{/if}
+				</button>
 			</div>
 		{/if}
 	</div>
